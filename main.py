733e382f18521fd08eab7bafa35b39d8f9eff0d9
Oscar <oscar.dodd1@education.nsw.gov.au>
2025-08-12 13:44:41 +1000

    Updated main.py
    -Changed a few things in main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 def select_option():
-    print("===Select Option===")
+    print("\n===Select Option===")
     print("1 - View Table")
     print("2 - Filter Table")
     print("3 - Visualise")
     print("4 - Exit")
     print("===================")
 
-    choice1 = input("Choice: ")
+    choice1 = input("\nChoice: ")
+
+    print("\n")
 
     if choice1 == "1":
-        print("a")
+        print("Choose table")
     elif choice1 == "2":
-        print("b")
+        print("Choose table and filter type")
     elif choice1 == "3":
-        print("c")
+        print("choose table to visualise")
     elif choice1 == "4":
         print("Exit")
     else:
