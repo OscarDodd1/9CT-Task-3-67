cad87f3b0984baba13e9f20d0ea46d2d87f0163e
Oscar <oscar.dodd1@education.nsw.gov.au>
2025-08-12 11:07:16 +1000

    Updated main.py
    -Added a choice interface to main.py

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 def select_option():
-    print()
+    print("===Select Option===")
+    print("1 - View Table")
+    print("2 - Filter Table")
+    print("3 - Visualise")
+    print("4 - Exit")
+    print("===================")
+
+    choice1 = input("Choice: ")
+
+    if choice1 == "1":
+        print("a")
+    elif choice1 == "2":
+        print("b")
+    elif choice1 == "3":
+        print("c")
+    elif choice1 == "4":
+        print("Exit")
+    else:
+        print("What...")
 
 select_option()
